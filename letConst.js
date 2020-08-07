aaecc7e5d2b097f5fa927c761e736a55a35015ba
exitdump <sheikhforid1026@gmail.com>
2020-08-08 01:06:47 +0600

    const let

---
+++ letConst.js
@@ -0,0 +1,16 @@
+ //const variable 
+ const wouldBe = "Nilima";
+ console.log(wouldBe)
+
+ const numbers = [12, 34];
+ numbers[1] = 88;
+ numbers.push(26);
+ console.log(numbers);
+
+ const fruits = {name: 'Banana', color: 'yellow'};
+ console.log(fruits);
+
+ //let variable
+ let patientName  = 'Mahbuba';
+ patientName = 'Fahmida';
+ console.log(patientName)
